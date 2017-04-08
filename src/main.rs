92d3c05b1627e157d355b4dce54cfe37443d7dff
RobGThai <thisisrobg@gmail.com>
2017-04-08 13:47:09 +0700

    Init.

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+      .expect("Failed to read line");
+
+    guess.pop();
+
+    match guess.parse::<i32>().unwrap().cmp(&secret_number) {
+      Ordering::Less    => println!("Too Small"),
+      Ordering::Greater => println!("Too Big"),
+      Ordering::Equal   => println!("You win!")
+    }
+
+}
